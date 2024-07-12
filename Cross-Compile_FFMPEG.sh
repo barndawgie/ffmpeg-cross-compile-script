7e06c969691a8d719abc5d2d6120bf7bf67224c6
Josh Barnard <barndawgie@users.noreply.github.com>
2024-07-12 12:00:21 -0700

    Initial stab at QSV

--- Cross-Compile_FFMPEG.sh
+++ Cross-Compile_FFMPEG.sh
@@ -66,6 +66,8 @@ libsvtav1_git="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
 libsvtav1_version="v2.1.2"
 ffnvcodec_git="https://github.com/FFmpeg/nv-codec-headers.git"
 ffnvcodec_release="n12.2.72.0"
+msdk_git="https://github.com/Intel-Media-SDK/MediaSDK.git"
+msdk_release="intel-mediasdk-21.3.5"
 libvmaf_git="https://github.com/Netflix/vmaf.git"
 libvmaf_release="v3.0.0"
 
@@ -429,6 +431,19 @@ pushd video || exit
     do_git_checkout $ffnvcodec_git $ffnvcodec_release ffnvcodec
     pushd ffnvcodec || exit
     make install PREFIX=$prefix
+    popd || exit
+
+    # Intel Media SDK (Quick Sync Video)
+    do_git_checkout $msdk_git $msdk_release MediaSDK
+    pushd MediaSDK || exit
+    mkdir -p build
+    cd build || exit
+    cmake -DCMAKE_TOOLCHAIN_FILE="$config_dir/toolchain-x86_64-w64-mingw32.cmake" \
+        -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SAMPLES=OFF -DBUILD_TUTORIALS=OFF -DBUILD_DISPATCHER=OFF \
+        ..
+    make -j $threads
+    make install
     popd || exit
 
     #libvmaf
