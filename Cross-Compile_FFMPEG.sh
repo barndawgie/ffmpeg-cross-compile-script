9985c6943aa99899e0df874f4b53071de2cbd6da
Josh Barnard <barndawgie@users.noreply.github.com>
2026-07-26 17:50:08 -0700

    Checking out LAME from SVN instead of downloading from SourceForge

--- Cross-Compile_FFMPEG.sh
+++ Cross-Compile_FFMPEG.sh
@@ -310,7 +310,7 @@ pushd audio || exit
     ./configure $configure_params --disable-gtktest --enable-nasm
     make -j $threads
     make install
-    popd || exit
+    popd ||
 
     #FDK: The Best AAC Codec for ffmpeg
     do_git_checkout $fdk_git $fdk_release fdk-aac
